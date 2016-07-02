d9a5695d8eed124cd45d1863671ab0a4d4251e56
Daniel Grace <thisgenericname@gmail.com>
2016-07-01 20:47:26 -0700

    Very early sources.  kOS detects it, so hurray?

---
+++ plugin/KIPCPlugin/Addon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using kOS.Suffixed;
+using kOS.AddOns;
+
+namespace kOS.Addons.KIPC
+{
+    [kOSAddon("KIPC")]
+    [kOS.Safe.Utilities.KOSNomenclature("KIPCAddon")]
+    public class Addon : Suffixed.Addon
+    {
+        public Addon(SharedObjects shared) : base (shared)
+        {
+            InitializeSuffixes();
+        }
+
+        public void InitializeSuffixes()
+        {
+
+        }
+
+        public override BooleanValue Available()
+        {
+            return true;
+        }
+    }
+}
