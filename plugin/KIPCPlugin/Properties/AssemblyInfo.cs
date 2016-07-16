204561be6f2e244c1e8eca7b3ab203561d329840
Daniel Grace <thisgenericname@gmail.com>
2016-07-16 10:59:23 -0700

    More build improvmeents and suport for NetKAN and AVC.

--- plugin/KIPCPlugin/Properties/AssemblyInfo.cs
+++ plugin/KIPCPlugin/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
 
 [assembly: KSPAssembly("KIPC", 0, 2)]
 [assembly: KSPAssemblyDependency("kOS", 0, 0)]
--- plugin/Postbuild.ps1
+++ plugin/Postbuild.ps1
@@ -1,15 +1,33 @@
 # powershell $(SolutionDir)\Postbuild.ps1 -BuildDir $(ProjectDir)$(OutDir) -KSPDir $(SolutionDir)..\KSP -OutputDir $(SolutionDir)..\Output -ClientGen $(SolutionDir)..\Python\venv\scripts
 Param (
-    [Parameter(Mandatory=$true, Position=0)]
+    # Location of build artifacts
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
     $BuildDir,
 
-    [Parameter(Mandatory=$true, Position=1)]
+    # Solution dir.  Also used to generate defaults for other arguments.
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $SolutionDir,
+
+    # Where to store final output.
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string]
     $OutputDir,
 
-    [Parameter(Mandatory=$true, Position=2)]
+    # Location of KSP
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string]
     $KSPDir,
 
-    [Parameter(Mandatory=$true, Position=3)]
+    # Location of build scripts
+    [Parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [string]
     $ScriptsDir
 )
 
@@ -20,22 +38,40 @@ $Languages = @(
 )
 
 $BuildDir = (Get-Item $BuildDir).FullName
+$SolutionDir = (Get-Item $SolutionDir).FullName
+
+if(-not $OutputDir) { $OutputDir = Join-Path $SolutionDir "..\output" }
+if(-not $KSPDir) { $KSPDir = Join-Path $SolutionDir "..\KSP" }
+if(-not $ScriptsDir) { $ScriptsDir = Join-Path $SolutionDir "..\python/venv/scripts" }
+
 $OutputDir = (Get-Item $OutputDir).FullName
 $KSPDir = (Get-Item $KSPDir).FullName
 $ScriptsDir = (Get-Item $ScriptsDir).FullName
 $ClientGen = Join-Path $ScriptsDir "krpc-clientgen"
 
+echo "BuildDir:    $BuildDir"
+echo "SolutionDir: $SolutionDir"
+echo "KSPDir:      $KSPDir"
+echo "OutputDir:   $OutputDir"
+echo "ScriptsDir:  $ScriptsDir"
+echo "ClientGen:   $ClientGen"
+
 # We're going to need a tempdir.  Make one
 $TempPath = [System.IO.Path]::GetTempPath()
 while($true) {
     $TempDir = Join-Path $TempPath ([System.IO.Path]::GetRandomFileName())
-    mkdir $TempDir -ErrorVariable DirErr -ErrorAction SilentlyContinue
+    mkdir $TempDir -ErrorVariable DirErr -ErrorAction SilentlyContinue | Out-Null
     if($DirErr) { continue }
     break
 }
 Register-EngineEvent -SourceIdentifier Powershell.Exiting -SupportEvent -Action {
-    rmdir $TempDir -Recurse -Verbose -Force
+    rmdir $TempDir -Recurse -Force
+    echo "Removed $TempDir"
 }
+echo "TempDir:     $TempDir"
+
+$ErrorActionPreference = "Stop"
+$WarningPreference = "Stop"
 
 # Create folder structure
 mkdir $TempDir/Output | Out-Null
@@ -46,11 +82,21 @@ mkdir $TempDir/DLLs | Out-Null
 
 # Copy DLLs.
 $BuildDLLs = gci $BuildDir -Filter "*.dll"
-$BuildDLLs | %{ Copy-Item $_.FullName $TempDir/Output/GameData/KIPC/Plugins -Verbose -Force }
-$BuildDLLs | %{ Copy-Item $_.FullName $TempDir/DLLs -Verbose -Force }
+$BuildDLLs | %{ Copy-Item $_.FullName $TempDir/Output/GameData/KIPC/Plugins -Force }
+$BuildDLLs | %{ Copy-Item $_.FullName $TempDir/DLLs -Force }
 
 $Dependencies = @(gci $KSPDir/GameData/KOS -Filter "*.dll" -Recurse) + @(gci $KSPDir/GameData/KRPC -Filter "*.dll" -Recurse)
-$Dependencies | %{ Copy-Item $_.FullName $TempDir/DLLs -Verbose -Force }
+$Dependencies | %{ Copy-Item $_.FullName $TempDir/DLLs -Force }
+
+# Create AVC version file
+$VersionInfo = $BuildDLLs | Where Name -Like "*KIPC*" | Select -First 1 | Select -ExpandProperty VersionInfo
+$AVCJson = Get-Content $SolutionDir/KIPC.version.template | ConvertFrom-Json
+$AVCJson.VERSION.MAJOR = $VersionInfo.FileMajorPart
+$AVCJson.VERSION.MINOR = $VersionInfo.FileMinorPart
+$AVCJson.VERSION.PATCH = $VersionInfo.FileBuildPart
+$AVCJson.VERSION.BUILD = $VersionInfo.FilePrivatePart
+$AVCJson | ConvertTo-Json -Compress | Set-Content $TempDir/Output/GameData/KIPC/KIPC.version 
+Copy-Item $TempDir/Output/GameData/KIPC/KIPC.version $SolutionDir
 
 # Build client libraries.
 $ClientDefs = "$TempDir/Output/Clients/clientdefs.json"
@@ -59,7 +105,6 @@ $HaveJSON = $false
 foreach($Language in $Languages) {
     $LangDir = Join-Path $TempDir/Output/Clients $Language.Lang
     $OutputFile = Join-Path $LangDir $Language.File
-    echo $OutputFile
     mkdir $LangDir | Out-Null
     if($HaveJSON) {
         $Args = @($Language.Lang, "KIPC", $ClientDefs)
@@ -67,17 +112,21 @@ foreach($Language in $Languages) {
         $HaveJSON = $true
         $Args = @($Language.Lang, "KIPC", "--ksp", $KSPDir, "--output-defs", $ClientDefs) + @($BuildDLLs | %{ $_.Name })
     }
-    echo ($ClientGen + " " + ($Args -Join " "))
+    echo ("[clientgen] " + ($Args -Join " "))
     &$ClientGen $args | Out-File $OutputFile
 }
 Pop-Location
 
-# zip the output folder
-Compress-Archive -Path $TempDir/Output/* -DestinationPath $TempDir/KIPC.zip -Force -Verbose
-Move-Item $TempDir/KIPC.zip $TempDir/Output/KIPC.zip
+# Zip the output folder
+$ZipName = "KIPC-$($VersionInfo.FileMajorPart).$($VersionInfo.FileMinorPart).$($VersionInfo.FileBuildPart).zip"
+Compress-Archive -Path $TempDir/Output/* -DestinationPath "$TempDir/Output/$ZipName" -Force -Verbose
+# Move-Item $TempDir/KIPC.zip $TempDir/Output/$ZipName
 
 # Move the output dir to where it should be.
-robocopy /MIR $Tempdir/Output $OutputDir
+robocopy /MIR $Tempdir/Output $OutputDir /NJH /NJS /NFL /NDL /NP
 
 # Mirror to KSP
-robocopy /MIR $OutputDir/Gamedata/KIPC $KSPDir/Gamedata/KIPC
+robocopy /MIR $OutputDir/Gamedata/KIPC $KSPDir/Gamedata/KIPC /NJH /NJS /NFL /NDL /NP
+
+# Remove temp directories
+Remove-Item -Recurse $TempDir
